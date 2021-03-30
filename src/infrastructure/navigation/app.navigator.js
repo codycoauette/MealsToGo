b8b5470fc23c1764fb48671bcf407287576a82c1
Cody Coauette <codycoauette@Codys-MacBook-Pro.local>
2021-03-30 15:17:17 -0500

    app progress

---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { SafeArea } from "../../components/utililty/safe-area.component";
+
+import { RestaurantsNavigator } from "./restaurants.navigator";
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
+
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={createScreenOptions}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+        <Tab.Screen name="Map" component={Map} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Text } from "react-native";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screens";
+
+const RestaurantStack = createStackNavigator();
+
+export const RestaurantsNavigator = () => {
+  return (
+    <RestaurantStack.Navigator
+      headerMode="none"
+      screenOptions={{
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
+    >
+      <RestaurantStack.Screen
+        name="Restaurants"
+        component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={() => <Text>Restaurant Detail</Text>}
+      />
+    </RestaurantStack.Navigator>
+  );
+};
